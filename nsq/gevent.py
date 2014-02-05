92d692b2af13a63029ada60b8895e831f066d897
Dan Lecocq <dan@moz.com>
2014-02-05 07:16:20 -0800

    Added the gevent client

---
+++ nsq/gevent.py
@@ -0,0 +1,9 @@
+'''A Gevent-based client'''
+
+from gevent import monkey
+monkey.patch_all()
+
+# The gevent client is actually the same as the synchronous client, just with
+# socket, thread, ssl, select and time patched. By importing this here, it makes
+# a gevent-compatible client available from nsq.gevent.Client
+from .client import Client
